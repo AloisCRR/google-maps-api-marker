9c4666bc9fd3b3473f7b44de12a3a53da9f8ffcc
Alois Carrera <aloiscarrera@gmail.com>
2021-02-05 20:54:55 -0500

    feat: :sparkles: Render map in div element

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
-console.log("Hello world in Snowpack");
+// import { Company } from "./Company";
+// import { User } from "./User";
+import { Loader } from "@googlemaps/js-api-loader";
+
+const loader = new Loader({
+  apiKey: String(import.meta.env.SNOWPACK_PUBLIC_GOOGLE_MAPS_API_KEY),
+  version: "weekly",
+  libraries: ["places", "visualization", "drawing", "geometry", "localContext"],
+});
+
+(async function () {
+  try {
+    await loader.load();
+
+    const mapDiv = document.querySelector<HTMLElement>("#map")!;
+
+    mapDiv.style.height = "100vh";
+
+    const mapOptions = {
+      zoom: 5,
+      center: {
+        lat: 0,
+        lng: 0,
+      },
+    };
+
+    return new google.maps.Map(mapDiv, mapOptions);
+  } catch (error) {
+    return console.log(error);
+  }
+})();
